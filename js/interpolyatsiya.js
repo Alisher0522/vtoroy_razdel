845e8e25e3985386450098f7eebe87dd4e70c6a7
Alisher <sherlock.homes.0522@gmail.com>
2024-01-18 19:20:35 +0500

    save afte gitignore

---
+++ js/interpolyatsiya.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+
+let incr = 10,
+	decr = 10;
+
+console.log(incr++, ++incr);
+console.log(decr--, --decr);
---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+console.log('hello alisher');
+const arr = [1, 2, 3];
+
+console.log(arr);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = '10';
+
+console.log(number);
+console.log(leftBorderWidth);
+
+const person = {
+	name: 'Alisher',
+	age: 25,
+	tel: 1422
+};
+
+const workers = ['alisher', 'iskandar', 'others'];
+
+workers.push('tillo');
+console.log(workers);
+
+person['gender'] = 'Male';
+person.tel = 9981;
+console.log(person, Object.keys(person), typeof(number));
+
+// const result = confirm('Tastiqlaysizmi?');
+// console.log(result);
+
+const answer = prompt('Yoshingiz nechida?', '');
+console.log(answer);
+
+
